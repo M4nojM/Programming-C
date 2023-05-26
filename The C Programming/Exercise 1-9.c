ea61599e97878774eff8c62376927232c9411ac8
Manoj M <manoj.mb.murali@gmail.com>
2023-05-26 16:57:45 +0530

    Edited Exercise 1-9 to handle multiple tabs

--- The C Programming/Exercise 1-9.c
+++ The C Programming/Exercise 1-9.c
@@ -2,11 +2,17 @@
 
 int main()
 {
-  int c;
+  int c, cprev;
 
   while((c = getchar()) != EOF){
+    
+    /* convert tabs into space */
     if (c == '\t') 
       c = ' ';
-    putchar(c);
+    /* putting value of c only if the current value and previous value not a space */
+    if (!(cprev == ' ' && c ==' '))
+      putchar(c);
+    /* assigning value of c into cache variable */
+    cprev = c;
   }
 }                         
